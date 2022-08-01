f869e91fa141418b943e4ce7afd1ae87ee13cb1f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-08-01 14:05:57 -0600

    added card with image to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+import { Card } from "react-bootstrap";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Card className='d-flex  justify-content-center'>
+        <Card.Title className='d-flex justify-content-center'>
+          Welcome
+        </Card.Title>
+        <p>
+          This is the store, please feel free to browse around. these items are
+          what there is so far
+        </p>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSsnXuiSjJ2zorNGfoISRlrBX7cZh03Kprhg&usqp=CAU' />
+      </Card>
+    </>
+  );
 }
